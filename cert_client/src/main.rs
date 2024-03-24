4aa4d04ee44b511e9d55a69cb50f6b0687e33916
Mason <109301662+CodedMasonry@users.noreply.github.com>
2024-03-24 14:01:27 -0400

    chore: Init slint UI

--- cert_client/src/main.rs
+++ cert_client/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()
 }
